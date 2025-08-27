bcc2ad3164483ee78929656ba1d1acfebec15646
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2025-08-27 21:47:54 +0200

    add hero section and body to homepage

--- home/models.py
+++ home/models.py
@@ -1,7 +1,54 @@
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.fields import RichTextField
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    hero_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Homepage hero image",
+    )
+    hero_title = models.CharField(
+        blank=True,
+        max_length=255,
+        help_text="Write a title for the site",
+    )
+    hero_text = models.CharField(
+        blank=True, max_length=255, help_text="Write an introduction for the site"
+    )
+    hero_cta = models.CharField(
+        blank=True,
+        verbose_name="Hero CTA",
+        max_length=255,
+        help_text="Text to display on Call to Action",
+    )
+    hero_cta_link = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Hero CTA link",
+        help_text="Choose a page to link to for the Call to Action",
+    )
+
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("hero_image"),
+                FieldPanel("hero_title"),
+                FieldPanel("hero_text"),
+                FieldPanel("hero_cta"),
+                FieldPanel("hero_cta_link"),
+            ],
+            heading="Hero section",
+        ),
+        FieldPanel("body"),
+    ]
